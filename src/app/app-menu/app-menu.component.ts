d0c379f09964a523a1a77f6e427540d6c4f3f3c7
LeandroTorresCoutinho <leandro.torres.coutinho@gmail.com>
2020-05-27 19:34:58 -1000

    adding style on sidenav

--- src/app/app-menu/app-menu.component.ts
+++ src/app/app-menu/app-menu.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class AppMenuComponent implements OnInit {
   @Input('sidenav')
-  sidenav: String;
+  sidenav: any;
   
   constructor() { }
 
